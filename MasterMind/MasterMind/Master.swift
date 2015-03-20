5825a5aaade366bb60502313ad4ac6b6733fd315
Julien Stefani <julienstefani@Juliens-Mac.local>
2015-03-20 02:46:29 -0700

    Ajout de Model

---
+++ MasterMind/MasterMind/Master.swift
@@ -0,0 +1,13 @@
+//
+//  Master.swift
+//  MasterMind
+//
+//  Created by Julien Stefani on 20/03/15.
+//  Copyright (c) 2015 Julien Stefani. All rights reserved.
+//
+
+import Cocoa
+
+class Master: Observable {
+        
+}
